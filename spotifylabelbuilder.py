46e6e8c8a58bb6d6c5e7ffc52afe6fe769fa9b69
Jeremy Castillo <jeremy.c@accessorypower.com>
2019-04-11 13:40:22 -0700

    Initial Commit

---
+++ spotifylabelbuilder.py
@@ -0,0 +1,36 @@
+import sys
+import spotipy
+import spotipy.util as util
+import configparser
+
+scope = 'user-read-recently-played user-top-read user-library-modify user-library-read playlist-modify-public playlist-read-collaborative user-read-email user-read-birthdate user-read-private user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming user-follow-read user-follow-modify'
+
+username = "Jeremy Maxwell Castillo"
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+token = util.prompt_for_user_token(username, scope, client_id=config['DEFAULT']['SPOTIFY_CLIENT_ID'], client_secret=config['DEFAULT']['SPOTIFY_CLIENT_SECRET'], redirect_uri='http://localhost')
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    tracks = sp.album_tracks(sys.argv[1])
+    musicDir = config['DEFAULT']['MUSIC_DIR']
+    currentTime = 0.0
+    fileContents = ""
+    separator = ""
+    for track in tracks['items']:
+        trackName = track['name']
+        trackDuration = float(track['duration_ms']) / 1000.0
+        fileContents += separator + "{0:.6f}".format(currentTime) + "\t" + "{0:.6f}".format(currentTime + trackDuration) + "\t" + trackName
+        currentTime += trackDuration
+        separator = "\n"
+    album = sp.album(sys.argv[1])
+    print(album)
+    year = album['release_date'].split('-')[0]
+    file = open(musicDir + album['artists'][0]['name'] + "\\" + album['name'] + " (" + str(year) + ")\\labels.txt", "w+")
+    file.write(fileContents)
+    file.close()
+else:
+    print("Can't get token for", username)
+
